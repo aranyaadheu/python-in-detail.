2d89c1122e84ab23b98c4544a667ede87e3ae10f
Aniruddha Aranya A Dheu <125354418+aranyaadheu@users.noreply.github.com>
2025-02-05 15:49:35 +0600

    Add files via upload

---
+++ python in detail/iterative statement prob.py
@@ -0,0 +1,10 @@
+### You want to print a message 
+# that tells the user to “try again” as long as the value of the attempt variable is 5 or less,
+#  and you want to increase the value of this variable by 1 each time it passes through the loop.
+
+attempt = 1 # starts from 1
+
+while attempt <= 5:
+    print("Try again!") 
+    attempt += 1 # increase attempt by 1. 
+
